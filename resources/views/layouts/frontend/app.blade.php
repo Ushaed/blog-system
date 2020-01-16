3a3c3cf9ffbf6fe958625231c7a292eef34fc0e6
ushaed <mdushaed29@gmail.com>
2020-01-16 14:10:30 +0600

    sweetalert added

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title> @yield('title') - {{ config('app.name', 'Laravel') }}</title>
+    <title> @yield('title') - {{ config('app.name', 'Blog') }}</title>
     <link rel="shortcut icon" href="{{asset('public/assets/backend/favicon.png')}}" type="image/x-icon">
 
     <link href="{{asset('public/assets/frontend/common-css/bootstrap.css')}}" rel="stylesheet">
